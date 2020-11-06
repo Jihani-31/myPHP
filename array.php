088eb030897e41ab5bdeab31155a370c23c329a1
Jihani-31 <jihaninanayakkara@gmail.com>
2020-11-07 03:19:43 +0530

    array

--- array.php
+++ array.php
@@ -3,7 +3,13 @@
  $array= array("mango","orange");
  echo $array[0];
 
- $array1=array(1,2, "jee");
+ $array1=array(1,2,20,12);
+
+ echo "<br>";
+ echo max($array1);
+ echo "<br>";
+ sort($array1);
+ echo print_r($array1);
 
  $array[]="banana";
  
